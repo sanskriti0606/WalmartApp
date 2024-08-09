3d5f4bc97703914405bb9467132f70dbe10ea86b
sanskriti0606 <104161059+sanskriti0606@users.noreply.github.com>
2024-08-09 12:34:49 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,14 +8,14 @@ import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   //Paste your firebase credentials in the given keys
-  apiKey: "AIzaSyCZWJizLoc5iDDhd-Daj2HLj25huuCyTF0",
-  authDomain: "walmartapp-f428a.firebaseapp.com",
-  databaseURL: "https://walmartapp-f428a-default-rtdb.firebaseio.com",
-  projectId: "walmartapp-f428a",
-  storageBucket: "walmartapp-f428a.appspot.com",
-  messagingSenderId: "318708647404",
-  appId: "1:318708647404:web:0508014a6e624d2bbcecc9",
-  measurementId: "G-0JGRPC7ECW"
+  // apiKey: "AIzaSyCZWJizLoc5iDDhd-Daj2HLj25huuCyTF0",
+  // authDomain: "walmartapp-f428a.firebaseapp.com",
+  // databaseURL: "https://walmartapp-f428a-default-rtdb.firebaseio.com",
+  // projectId: "walmartapp-f428a",
+  // storageBucket: "walmartapp-f428a.appspot.com",
+  // messagingSenderId: "318708647404",
+  // appId: "1:318708647404:web:0508014a6e624d2bbcecc9",
+  // measurementId: "G-0JGRPC7ECW"
 };
 
 // Initialize Firebase
